849e0700b42402c63425853b7bf531c5db5d45e4
Arunprakash <arunprakashnv@gmail.com>
2023-02-24 15:08:39 +0530

    full project

---
+++ SimpleTestProject/Hooks/DriverSetup.cs
@@ -0,0 +1,33 @@
+﻿namespace SimpleTestProject.Hooks
+{
+    using BoDi;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using System.IO;
+    using System.Reflection;
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class DriverSetup
+    {
+        private IObjectContainer _objectContainer;
+        public IWebDriver Driver;
+
+        public DriverSetup(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            
+            string outputDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);                          
+            Driver = new ChromeDriver();
+            _objectContainer.RegisterInstanceAs(Driver);
+        }
+
+    }
+}
+
